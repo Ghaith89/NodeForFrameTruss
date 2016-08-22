714aee1d452247f49b2b38fb5fdc5dc843c7712b
Ghaith <ghaith-tish@hotmail.com>
2016-08-23 00:53:01 +0200

    NodeCode

---
+++ NodeForFreeformTruss/NodeForFreeformTruss/NodeForFreeformTrussComponent.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+
+namespace Node_FrameTruss
+{
+    public class NodeFrameTrussComponent : GH_Component
+    {
+        /// <summary>
+        /// Each implementation of GH_Component must provide a public 
+        /// constructor without any arguments.
+        /// Category represents the Tab in which the component will appear, 
+        /// Subcategory the panel. If you use non-existing tab or panel names, 
+        /// new tabs/panels will automatically be created.
+        /// </summary>
+        public NodeFrameTrussComponent()
+          : base("Node_FrameTruss", "Nickname",
+              "Description",
+              "Extra", "Subcategory")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddLineParameter("BeamAxesList", "BeAxLi", "TheLinesOf The Beams", GH_ParamAccess.list);
+            pManager.AddPointParameter("NodesPositions", "NPos", "The Positions of the nodes", GH_ParamAccess.list);
+            pManager.AddNumberParameter("NodeRadius", "NRad", "The Radius of the nodes", GH_ParamAccess.item);
+            pManager.AddNumberParameter("BeamRadius", "BRad", "The Radius of the Crossection", GH_ParamAccess.item);
+            pManager.AddNumberParameter("DistanceFromCenter", "DiFrCen", "The dstance between the end of the beam and the node position", GH_ParamAccess.item);
+
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddBrepParameter("ResultedNodesList", "ReNoLi", "ResultedNodesList", GH_ParamAccess.list);
+            pManager.AddNumberParameter("BeamConnectivity", "BeCon", "The Topoogy of the nodes and Beams", GH_ParamAccess.tree);
+            pManager.AddBrepParameter("BeamGeometry", "BeGeo", "The Geometry Of The Connected Beams", GH_ParamAccess.tree);
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object can be used to retrieve data from input parameters and 
+        /// to store data in output parameters.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            //PlaceHolders
+            List<Line> BeamAxisList = new List<Line>();
+            List<Point3d> NodePositions = new List<Point3d>();
+            double NodeRadius = double.NaN;
+            double BeamRadius = double.NaN;
+            double DisFromCenter = double.NaN;
+
+            //AsseignValuesToInputs
+            if (!DA.GetDataList(0, BeamAxisList)) { return; }
+            if (!DA.GetDataList(1, NodePositions)) { return; }
+            if (!DA.GetData(2, ref NodeRadius)) { return; }
+            if (!DA.GetData(3, ref BeamRadius)) { return; }
+            if (!DA.GetData(4, ref DisFromCenter)) { return; }
+
+            //Modelling
+            NodeGeometry Node = new NodeGeometry();
+            
+            List<List<Line>> Conncted = new List<List<Line>>();
+            List<List<int>> Topo = new List<List<int>>();
+            List<List<Brep>> BeamConn = new List<List<Brep>>();
+
+            //OutPutsValues
+            List<Brep> NodeList = new List<Brep>();
+            foreach (Point3d i in NodePositions)
+            {
+                //Define The Elements Connected To each Node
+                List<int> ConnectedFaces = new List<int>();
+                List<Line> BeamConnectivity = Node.BeamConnectivity(i, BeamAxisList, out ConnectedFaces);
+                Conncted.Add(BeamConnectivity);
+                Topo.Add(ConnectedFaces);
+                List<Brep> CrosssectionsList = new List<Brep>();
+                foreach (Line n in BeamConnectivity)
+                {
+                    Brep Crossection = Node.BeamGeometry(i, n, BeamRadius, DisFromCenter);
+                    CrosssectionsList.Add(Crossection);
+                }
+                Brep NodeGeometry = Node.NodeBrep(i, NodeRadius, CrosssectionsList);
+                NodeList.Add(NodeGeometry);
+                BeamConn.Add(CrosssectionsList);
+            }
+            //OutPut The CrossectionGeometry Tree
+            Grasshopper.DataTree<Brep> Cyl = new Grasshopper.DataTree<Brep>();
+            for (int i = 0; i < BeamConn.Count; i++)
+            {
+                Cyl.AddRange(BeamConn[i], new Grasshopper.Kernel.Data.GH_Path(i));
+            }
+
+            //OutPut The BeamTopology Tree
+            Grasshopper.DataTree<int> TopoBeam = new Grasshopper.DataTree<int>();
+            for (int i = 0; i < Topo.Count; i++)
+            {
+                TopoBeam.AddRange(Topo[i], new Grasshopper.Kernel.Data.GH_Path(i));
+            }
+
+            DA.SetDataList(0, NodeList);
+            DA.SetDataTree(1, TopoBeam);
+            DA.SetDataTree(2, Cyl);
+        }
+
+        /// <summary>
+        /// Provides an Icon for every component that will be visible in the User Interface.
+        /// Icons need to be 24x24 pixels.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                // You can add image files to your project resources and access them like this:
+                //return Resources.IconForThisComponent;
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Each component must have a unique Guid to identify it. 
+        /// It is vital this Guid doesn't change otherwise old ghx files 
+        /// that use the old ID will partially fail during loading.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("{0ae146c6-1cfe-4167-9adc-73feb1422d39}"); }
+        }
+    }
+}
---
+++ NodeForFreeformTruss/NodeForFreeformTruss/NodeGeometry.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+
+namespace NodeForFreeformTruss
+{
+    class NodeGeometry
+    {
+        //List<int> ConnBeAxeInt;
+        //public double BeamRad;
+        public List<Line> BeamConnectivity(Point3d NodePosition, List<Line> AxesList, out List<int> ConnBeAxeInt)
+        {
+            List<Line> ConnectedAxes = new List<Line>();
+            ConnBeAxeInt = new List<int>();
+            int Counter = 0;
+            foreach (Line Li in AxesList)
+            {
+                Counter++;
+                Point3d PoSt = Li.PointAt(0);
+                Point3d PoEd = Li.PointAt(1);
+                if (PoSt.DistanceTo(NodePosition) <= Rhino.RhinoDoc.ActiveDoc.ModelAbsoluteTolerance)
+                {
+                    ConnectedAxes.Add(Li);
+                    ConnBeAxeInt.Add(Counter);
+                }
+                if (PoEd.DistanceTo(NodePosition) <= Rhino.RhinoDoc.ActiveDoc.ModelAbsoluteTolerance)
+                {
+                    ConnectedAxes.Add(Li);
+                    ConnBeAxeInt.Add(Counter);
+                }
+            }
+
+            return ConnectedAxes;
+        }
+
+        public Brep BeamGeometry(Point3d Cen, Line BeamAxis, double BeamRad, double distanceFromCenter)
+        {
+            Line CorrectedLen = ShrinkBeam(Cen, BeamAxis, distanceFromCenter);
+
+            Point3d St = CorrectedLen.PointAt(0);
+            Point3d Ed = CorrectedLen.PointAt(1);
+            Vector3d LiVec = Ed - St;
+            Plane plaStrt = new Plane(St, LiVec);
+            Plane plaEd = new Plane(Ed, LiVec);
+
+            List<Curve> LoftCir = new List<Curve>();
+            Circle StCir = new Rhino.Geometry.Circle(plaStrt, BeamRad);
+            Curve Stcrve = StCir.ToNurbsCurve();
+            LoftCir.Add(Stcrve);
+            Circle EdCir = new Rhino.Geometry.Circle(plaEd, BeamRad);
+            Curve Edcrve = EdCir.ToNurbsCurve();
+            LoftCir.Add(Edcrve);
+            Brep[] BeamLi = Rhino.Geometry.Brep.CreateFromLoft(LoftCir, Point3d.Unset, Point3d.Unset, LoftType.Tight, false);
+            Brep Beam = BeamLi[0];
+            Brep CapBeam = Beam.CapPlanarHoles(0.1);
+
+            return CapBeam;
+        }
+
+        public Brep AdvancedBeamGeometry(Point3d Cen, Line BeamAxis, double BeamRad, double MidRad, double inclination, double distanceFromCenter)
+        {
+            Line CorrectedLen = ShrinkBeam(Cen, BeamAxis, distanceFromCenter);
+
+            Point3d St = CorrectedLen.PointAt(0);
+            Point3d Mid = CorrectedLen.PointAt(0.5);
+            Point3d Ed = CorrectedLen.PointAt(1);
+
+            Vector3d LiVec = Ed - St;
+            Plane plaStrt = new Plane(St, LiVec);
+            Plane plaEd = new Plane(Ed, LiVec);
+            Plane plaMid = new Plane(Mid, LiVec);
+
+            Circle MidCir = new Rhino.Geometry.Circle(plaMid, MidRad);
+            Curve mid = MidCir.ToNurbsCurve();
+
+            Vector3d Up = St - Mid;
+            Up.Unitize();
+            Vector3d Down = Ed - Mid;
+            Down.Unitize();
+
+            double Dist = BeamAxis.Length;
+            double HalDis = Dist / 2;
+            double SiDis = Dist / 6;
+            double TrDis = HalDis - SiDis;
+
+            Vector3d Tr01 = Up * TrDis;
+            Vector3d TrIn = SiDis * Up;
+            Transform Uptr = Transform.Translation(Tr01);
+            Transform Intr = Transform.Translation(TrIn);
+
+            List<Curve> LoftList = new List<Curve>();
+            Plane plaEd00 = new Plane(Mid, LiVec);
+            Circle PlEdCir00 = new Rhino.Geometry.Circle(plaEd00, MidRad);
+            Curve PlEdCir0 = PlEdCir00.ToNurbsCurve();
+            LoftList.Add(PlEdCir0);
+            Mid.Transform(Uptr);
+            Plane plaEd01 = new Plane(Mid, LiVec);
+            Circle PlEdCir01 = new Rhino.Geometry.Circle(plaEd01, MidRad);
+            Curve PlEdCir01Crv = PlEdCir01.ToNurbsCurve();
+            LoftList.Add(PlEdCir01Crv);
+            Mid.Transform(Intr);
+            Plane plaEd02 = new Plane(Mid, LiVec);
+            Circle PlEdCir02 = new Rhino.Geometry.Circle(plaEd02, inclination);
+            Curve PlEdCir02Crv = PlEdCir02.ToNurbsCurve();
+            LoftList.Add(PlEdCir02Crv);
+            Plane plaEdFinal = new Plane(St, LiVec);
+            Circle PlEdCirFi = new Rhino.Geometry.Circle(plaEdFinal, inclination);
+            Curve PlEdCirFiCrv = PlEdCirFi.ToNurbsCurve();
+            LoftList.Add(PlEdCirFiCrv);
+            Brep[] BeamLi = Rhino.Geometry.Brep.CreateFromLoft(LoftList, Point3d.Unset, Point3d.Unset, LoftType.Tight, false);
+            Brep LoftHalf = BeamLi[0];
+
+            Transform Mirror = Transform.Mirror(plaEd00);
+
+            List<Brep> JoinBrepList = new List<Brep>();
+            JoinBrepList.Add(LoftHalf);
+
+            Brep LoftHalf01 = LoftHalf;
+            LoftHalf01.Transform(Mirror);
+            JoinBrepList.Add(LoftHalf01);
+
+            Brep[] Beam = Brep.JoinBreps(JoinBrepList, 0.1);
+
+            Brep OpenBeam = Beam[0];
+
+            Brep CapBeam = OpenBeam.CapPlanarHoles(0.1);
+
+            return CapBeam;
+        }
+
+        public Line ShrinkBeam(Point3d Center, Line BeamAxe, double distanceFromCenter)
+        {
+            Point3d StPt = BeamAxe.PointAt(0);
+            Point3d EdPt = BeamAxe.PointAt(1);
+            Vector3d LiVec = EdPt - StPt;
+            Vector3d TrVec;
+            Point3d EndSide;
+            if (StPt.DistanceTo(Center) <= Rhino.RhinoDoc.ActiveDoc.ModelAbsoluteTolerance)
+            {
+                TrVec = LiVec;
+                EndSide = EdPt;
+            }
+            else
+            {
+                TrVec = LiVec * -1;
+                EndSide = StPt;
+            }
+            TrVec.Unitize();
+            Transform Tr = Transform.Translation(TrVec * distanceFromCenter);
+            Point3d TrPt = new Point3d(Center);
+
+            TrPt.Transform(Tr);
+
+            Line Corrected = new Line(TrPt, EndSide);
+
+            return Corrected;
+        }
+
+        public Brep NodeBrep(Point3d SphereCenter, double Radius, List<Brep> ListBeams)
+        {
+            Sphere NodeSphere = new Rhino.Geometry.Sphere(SphereCenter, Radius);
+            Brep SpBrep = NodeSphere.ToBrep();
+
+            Brep[] Node01 = Rhino.Geometry.Brep.CreateBooleanDifference(SpBrep, ListBeams[0], 0.1);
+            Brep Node01Brep = Node01[0];
+            List<Brep> DifferenceBreps = new List<Brep>();
+            DifferenceBreps.Add(Node01Brep);
+
+            for (int i = 1; i < ListBeams.Count; i++)
+            {
+                Brep[] Node02 = Rhino.Geometry.Brep.CreateBooleanDifference(DifferenceBreps[i - 1], ListBeams[i], 0.1);
+                DifferenceBreps.Add(Node02[0]);
+            }
+
+            int BrepIndex = DifferenceBreps.Count - 1;
+            return DifferenceBreps[BrepIndex];
+        }
+    }
+}
+
